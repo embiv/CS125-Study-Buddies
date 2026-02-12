271dd71f01e17d5f6f02674a9bac738a9c1b168e
katlynferrer <katlynferrer9@gmail.com>
2026-02-12 01:32:34 -0800

    added function get_closest_libraries for easier navigation

--- location.py
+++ location.py
@@ -12,6 +12,23 @@ def load_library(path):
 
     return library_name, (library_latitude, library_longitude)
 
+def get_closest_libraries(user_location, libraries):
+    for library_path in libraries:
+        load_library(library_path)
+    
+    # stores libraries from closest to farthest
+    results = []
+
+    # calculates the distance between the user and each library
+    for library_name, library_location in libraries:
+        # haversine: calculates distance between two points on a sphere
+        distance = haversine(user_location, library_location, unit=Unit.MILES)
+        results.append((library_name, distance))
+
+    results.sort(key=lambda x: x[1])
+    return results
+
+
 def main():
     # user_location = (latitude, longitude)
     user_location = (33.646, -117.843)
@@ -21,16 +38,7 @@ def main():
         load_library(r"C:\Users\katly\CS125-Study-Buddies\Study Spots\Science_Library.json")
     ]
 
-    # sorted by closest distance
-    results = []
-
-    # calculates the distance between the user and each library
-    for library_name, library_location in libraries:
-        # haversine: calculates distance between two points on a sphere
-        distance = haversine(user_location, library_location, unit=Unit.MILES)
-        print(library_name, round(distance, 2), "miles")
-
-    results.sort(key=lambda x: x[1])
+    results = get_closest_libraries(user_location, libraries)
 
     for library_name, library_distance in results:
         print(library_name, round(library_distance, 2), "miles")
